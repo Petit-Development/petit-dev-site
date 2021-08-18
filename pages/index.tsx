a133daedf1fb48911efa701086db1743e281af71
Joshua Petitma <joshua@petit.dev>
2021-08-18 15:05:39 -0400

    Modify colors to be darker

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,21 +10,21 @@ export default function Home() {
         <meta name="description" content="Freelance Web and Mobile App Development" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div>
-          <Image alt="Logo" width={485} height={220} src="/logo.svg" />
-        </div>
-        <h3>Freelance Development for Mobile and Web Applications</h3>
-        <h2>
-          <a className={styles.link} href="https://github.com/petit-development">Github</a>
-        </h2>
-        <h2>
-          <a className={styles.link} href="mailto: joshua@petit.dev">Email</a>
-        </h2>
-        <h2>
-          <a className={styles.link} href="https://www.youtube.com/channel/UClo6j1DhtvHIKBPcsmCgWlg/featured">Youtube</a>
-        </h2>
-      </main>
+        <main className={styles.main}>
+          <div>
+            <Image alt="Logo" width={485} height={220} src="/Whole Logo Dark.svg" />
+          </div>
+          <h3>Freelance Development for Mobile and Web Applications</h3>
+          <h2>
+            <a className={styles.link} href="https://github.com/petit-development">Github</a>
+          </h2>
+          <h2>
+            <a className={styles.link} href="mailto: joshua@petit.dev">Email</a>
+          </h2>
+          <h2>
+            <a className={styles.link} href="https://www.youtube.com/channel/UClo6j1DhtvHIKBPcsmCgWlg/featured">Youtube</a>
+          </h2>
+        </main>
     </div>
   )
 }

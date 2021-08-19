cf33d74740019af1f97054b9ec14b265b1ae0bb7
Joshua Petitma <joshua@petit.dev>
2021-08-19 12:35:20 -0400

    Add drop shadow onto main part of page

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,27 +5,35 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-      <div>
         <Head>
           <title>Petit Development</title>
         </Head>
-        <Image className={styles.logo} alt="Logo" width={512} height={137} src="/Whole Logo Dark.svg" />
-      </div>
-      <div className={styles.description}>
-      <h3> Petit Development, LLC provides
-        <span className={styles.secondary}> Mobile & Web application development</span> for <span className={styles.secondary}> Small to Medium sized Businesses</span>. Petit.dev contributes to the Open Source Community in a variety of ecosystems such as Flutter, Node, Java, Golang and more.</h3>
+      <div className={styles.top}>
+          <Image className={styles.logo} alt="Logo" width={512} height={137} src="/Whole Logo Dark.svg" />
+          <div className={styles.description}>
+            <h3> Petit Development, LLC provides
+              <span className={styles.secondary}> Mobile & Web application development</span> for <span className={styles.secondary}> Small to Medium sized Businesses</span>. Petit.dev contributes to the Open Source Community in a variety of ecosystems such as Flutter, Node, Java, Golang and more.</h3>
+          </div>
       </div>
       <main className={styles.main}>
-        <h2>
-          <a className={styles.link} href="https://github.com/petit-development">Github</a>
-        </h2>
-        <h2>
-          <a className={styles.link} href="mailto: joshua@petit.dev">Email</a>
-        </h2>
-        <h2>
-          <a className={styles.link} href="https://www.youtube.com/channel/UClo6j1DhtvHIKBPcsmCgWlg/featured">Youtube</a>
-        </h2>
+        <div>
+          Gif/Video location
+        </div>
+        <div className={styles.info}>
+          Information About what is on this Tab
+        </div>
       </main>
+      <footer className={styles.nav}>
+        <div>
+          YOUTUBE
+        </div>
+        <div>
+          FREELANCE
+        </div>
+        <div>
+          GITHUB
+        </div>
+      </footer>
     </div>
   )
 }
